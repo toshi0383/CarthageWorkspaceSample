09c863d0473a1a667788c019c7c45f27231d48f3
toshi0383 <t.suzuki326@gmail.com>
2017-04-21 09:39:31 +0900

    Implement SampleLibrary/Sample.swift

--- CarthageWorkspaceSample/ViewController.swift
+++ CarthageWorkspaceSample/ViewController.swift
@@ -7,19 +7,11 @@
 //
 
 import UIKit
+import SampleLibrary
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        Sample().run()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
---
+++ SampleLibrary/Sample.swift
@@ -0,0 +1,19 @@
+//
+//  Sample.swift
+//  CarthageWorkspaceSample
+//
+//  Created by 鈴木 俊裕 on 2017/04/21.
+//  Copyright © 2017 Toshihiro Suzuki. All rights reserved.
+//
+
+import Foundation
+
+open class Sample {
+    public init() {}
+    public var c = ""
+    public func run() {
+        let a = "boo"
+        let b = "bar"
+        self.c = "\(a)\(b)"
+    }
+}
