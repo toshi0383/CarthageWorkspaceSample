bc1a020a5ac371f77747c6750e8789c6976d97a3
toshi0383 <t.suzuki326@gmail.com>
2017-04-21 12:12:29 +0900

    Install SampleLibrary 1.0.0 via Carthage

--- CarthageWorkspaceSample/ViewController.swift
+++ CarthageWorkspaceSample/ViewController.swift
@@ -7,19 +7,14 @@
 //
 
 import UIKit
+import SampleLibrary
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let s = Sample()
+        s.run()
+        print(s.c)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
